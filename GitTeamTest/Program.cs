bbda21e93bb94622af5b6518127e2c94ba0e0279
LukasCaha <lukas.caha@outlook.com>
2019-11-08 22:48:05 +0100

    Add project files.

---
+++ GitTeamTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTeamTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj codebrew");
+        }
+    }
+}
